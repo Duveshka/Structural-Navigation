98215486367178e5c80ab8f99f5e07fb97ba1fea
duveshka <duveshka@mail.ru>
2021-11-23 13:11:57 +0300

    Add project files.

---
+++ Structural Navigation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Structural_Navigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
